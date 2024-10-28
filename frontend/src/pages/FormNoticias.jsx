aa3aef8743eed1ced608197bf51fe39d3e2f3fd0
Felipe Fernandes Alves <felipefernanalves@gmail.com>
2024-10-28 17:08:08 -0300

    Update FormNoticias.jsx

--- frontend/src/pages/FormNoticias.jsx
+++ frontend/src/pages/FormNoticias.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import "./Atendimento.css";
 import Cookie from "js-cookie";
@@ -8,21 +9,80 @@ const css = `
 `
 
 export default function Atendimeneto() {
+  const [content, setContent] = useState([]);
+
+  class Component {
+    constructor(name, value) {
+      this.name = name;
+      this.value = value;
+    }
+  }
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    const newComponent = new Component(e.target.name, e.target.value);
+    const newContent = content.concat([newComponent]);
+
+    setContent(newContent);
+  }
+
+  const moveContent = (e) => {
+    e.preventDefault();
+    const index = e.target.value;
+    if (index <= 0) return;
+
+    const dir = e.target.dir
+
+    const temp = content[index + dir];
+    const target = content[index];
+    const copy = content.copyWithin();
+    
+    copy[index + dir] = target;
+    copy[index] = temp;
+
+    setContent(copy);
+  }
+
   return (
     <Layout connected={Cookie.get("user")}>
       <style>{css}</style>
       <div className="atendimentoWrapper">
         <form>
           <h1>Notícias</h1>
-          <label>Título:</label>
-          <input type="text" name="title" />
-          <label>Autor:</label>
-          <input type="text" name="author" />
-          <label>Conteúdo:</label>
-          <textarea></textarea>
+          <div>
+            <label>Título:</label>
+            <input type="text" name="title" />
+            <label>Autor:</label>
+            <input type="text" name="author" />
+          </div>
+          
+          <div>
+            <div>
+              <button onClick={onClickHandler} name="subtitulo">Adicionar Subtítulo</button>
+              <button onClick={onClickHandler} name="imagem">Adicionar Imagem</button>
+            </div>
+            <div>
+              {content.map((c, key) => {
+                return(
+                  <div key={key}>
+                    <label htmlFor={c.name + c.key}> {c.name} </label>
+                    <input type="text" name={c.name} id={c.name + key} />
+                    <div>
+                      <button type="button" onClick={moveContent} value={key} dir={-1}>Up</button>
+                      <button type="button" onClick={moveContent} value={key} dir={1}>Down</button>
+                      <button type="button">Delete</button>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
           <input className="submitButton" type="submit" value="Enviar" />
         </form>
       </div>
+
+      
     </Layout>
   );
 }
